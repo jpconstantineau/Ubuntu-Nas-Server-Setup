b86e6b376f66b17d22676e1cf845b1a2abde0230
jpconstantineau <jpconstantineau@gmail.com>
2018-02-03 19:48:11 -0600

    Added installs

--- scripts/setupremote.sh
+++ scripts/setupremote.sh
@@ -1,4 +1,20 @@
-apt-get update
-apt-get upgrade
-apt install openssh-server
-apt install git zfs pv mbuffer lzop 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install openssh-server
+sudo apt-get install git
+sudo apt install zfs
+sudo apt install pv mbuffer lzop
+sudo apt-get install libconfig-inifiles-perl
+sudo apt-get install lm-sensors
+
+cd /opt
+sudo git clone https://github.com/jimsalterjrs/sanoid.git
+sudo ln /opt/sanoid/sanoid /usr/sbin/
+sudo ln /opt/sanoid/syncoid /usr/sbin/
+sudo mkdir -p /etc/sanoid
+sudo cp /opt/sanoid/sanoid.conf /etc/sanoid/sanoid.conf
+sudo cp /opt/sanoid/sanoid.defaults.conf /etc/sanoid/sanoid.defaults.conf
+
+echo sudo nano /etc/sanoid/sanoid.conf
+
+sudo apt-get install openvpn
