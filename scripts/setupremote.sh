02da19c36822b6610b3654968cc384fec048cdb0
Pierre Constantineau <jpconstantineau@gmail.com>
2018-02-06 20:03:33 -0600

    updated import file

--- scripts/setupremote.sh
+++ scripts/setupremote.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-source functions.sh
+
+. functions.sh
+
 host=$(hostname -f)
 
     while true; do
---
+++ scripts/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git config --global user.name ""
+git config --global user.email "@gmail.com"
+git clone https://github.com/jpconstantineau/Ubuntu-Nas-Server-Setup.git
+cd Ubuntu-Nas-Server-Setup
+cd scripts
+./setup.sh
